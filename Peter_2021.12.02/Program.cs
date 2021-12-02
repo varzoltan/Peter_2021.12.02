7a473c098efa62888f5cd9e547f0a7be3d00dac8
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-12-02 15:24:27 +0100

    Add project files.

---
+++ Peter_2021.12.02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Peter_2021._12._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //típus nev érték vagy kifejezés
+            //Karaktertípus:1
+            char karakter = 'a';
+            char karakter2 = karakter;
+
+            //Karaktertípus:2:String
+
+            Console.WriteLine(karakter + ":" + karakter2);
+            Console.ReadKey();
+
+        }
+    }
+}
