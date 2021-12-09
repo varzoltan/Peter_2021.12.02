5f422d7f2e1978878926fb4be60f71e1d2c76ea7
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-12-09 16:01:59 +0100

    16:01

--- Peter_2021.12.02/Program.cs
+++ Peter_2021.12.02/Program.cs
@@ -31,6 +31,30 @@ namespace Peter_2021._12._02
             Console.Write("Integer maximumértéke:" + ertek2+"\n");
             Console.WriteLine("Long minimumértéke:" + ertek3);
             Console.Write("Long maximumértéke:" + ertek4);
+
+            //boolean: True;False;0;1
+            bool igaz = true;
+            bool hamis = false;
+            int a = 2;
+            if(a != 2)
+            {
+                Console.WriteLine(igaz + "Igaz");
+                //a = 3;
+            }
+            else
+            {
+                igaz = false;
+                Console.WriteLine(igaz + "Hamis");
+                
+            }
+
+            //double: duplapontos lebegőszám!
+            int b = 10;
+            int c = 100;
+            double dupla = b * c;
+            //b = dupla / c;
+            Console.WriteLine("Eredmény: " + dupla);
+            Console.WriteLine("Eredmény: " + dupla/10000);
             Console.ReadKey();
 
         }
